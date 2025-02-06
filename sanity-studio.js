1d665f1f5468a5e66ce9d6003b79b8e464d6e75c
lautaro <lautaronuriel@gmail.com>
2025-02-05 21:36:57 -0300

    images width reduced to 600px

--- sanity-studio.js
+++ sanity-studio.js
@@ -22,7 +22,7 @@ function makeImgUrl(ref) {
   const id = parts[1];
   const dimentions = parts[2];
   const format = parts[3];
-  const optimizedSize = window.innerWidth <= 800 ? "&w=800" : "";
+  const optimizedSize = window.innerWidth <= 800 ? "&w=600" : "";
   return `https://cdn.sanity.io/images/${studioId}/${studioDataset}/${id}-${dimentions}.${format}?fm=webp${optimizedSize}`;
 }
 
